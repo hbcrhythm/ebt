c30aa4b446c4499f5d6fedcbe0fd02138d25e90a
hbc <727979420@qq.com>
2018-03-25 22:27:17 +0800

    fix bug

--- src/ebt_random_selector.erl
+++ src/ebt_random_selector.erl
@@ -21,7 +21,7 @@ do_evaluate(#ebt_node{id = Id, childs = Childs, param = #{static := SParam}}) ->
     case LastActiveNode of
         undefined when SParam =:= [] ->
             Random = rand:uniform(length(Childs)),
-            ActiveChild = lists:nth(Random, length(Childs)),
+            ActiveChild = lists:nth(Random, Childs),
             Data2 = Data#{active_child => ActiveChild},
             ?EBT_NODE_DATA(Id, Data2),
             true;
@@ -36,7 +36,7 @@ do_evaluate(#ebt_node{id = Id, childs = Childs, param = #{static := SParam}}) ->
                         undefined ->
                             false;
                         ActiveId ->
-                            ActiveChild = lists:nth(ActiveId, length(Childs)),
+                            ActiveChild = lists:nth(ActiveId, Childs),
                             Data2 = Data#{active_child => ActiveChild},
                             ?EBT_NODE_DATA(Id, Data2),
                             true
